a3a761ca89c644b83750399d3229bb473e2b80a4
Koril33 <dingjinghui33@163.com>
2020-04-21 15:56:33 +0800

    注释+命令行参数解析

--- huaban_by_requests.py
+++ huaban_by_requests.py
@@ -4,16 +4,26 @@ import requests
 import sys
 import concurrent.futures
 import time
+import argparse
 
+# 命令行参数
+parser = argparse.ArgumentParser(description='爬取花瓣网图片')
+parser.add_argument('--url', type=str, required=True, help='画板的网址')
+parser.add_argument('--max', type=int, default=10, help='下载图片的最大数量（默认10张）')
+parser.add_argument('--path', type=str, default='huaban_pictures', help='创建的文件夹的名字，用来保存下载图片（默认为‘huaban_pictures’）')
+args = parser.parse_args()
 
-# main_page = 'https://huaban.com/discovery/beauty/?k997zx3r&max=3104931188&limit=20&wfl=1'
-main_page = 'https://huaban.com/discovery/beauty/'
+main_page = args.url
+pic_max = args.max
+path_name = args.path
 
-# main_page = 'https://huaban.com/boards/60315823/'
+# 测试使用的网站
+# main_page = 'https://huaban.com/discovery/beauty/'
+# main_page = 'https://huaban.com/explore/manhuarenwu'
+# main_page = 'https://huaban.com/boards/37776086/'
+# main_page = 'https://huaban.com/favorite/design/'
 
-# main_page = 'https://huaban.com/discovery/men/'
-
-
+# 构造请求头
 my_header = {
 	'Accept': 'application/json',
 	'Accept-Encoding': 'gzip, deflate, br',
@@ -32,110 +42,161 @@ my_header = {
 
 
 def create_url_l(url):
-    if url.endswith('/'):
-        return url + '?&max='
-    else:
-        return url.split('/?', maxsplit=1)[0] + '/?&max='
+	"""构造页面url的左半部分
+	:param url: 用户传入的url
+	:return: 构造后的url
+	"""
 
+	if url.endswith('/'):
+		return url + '?&max='
+	else:
+		return url.split('/?', maxsplit=1)[0] + '/?&max='
+
+# 图片下载url，左半部分为定值
 pic_url_l = 'http://img.hb.aicdn.com/'
-
+# 页面url左半部分，通过create_url_l函数构造
 page_url_l = create_url_l(main_page)
-
+# 页面url右半部分
 page_url_r = '&limit=20&wfl=1'
-
+# 所有图片的下载地址
 all_urls = []
 
-pic_max = 1000
 
 def is_board_or_discovery(url):
+	"""判断下载的页面是画板（board）还是发现（discovery）
+	一般'board'以数字结尾，'discovery'以字母结尾
+	:param url: 用户传入的url
+	:return: 返回标志
+	"""
+
 	tag = url.split('/')[-2]
 	if tag.isdigit():
 		return 'board'
 	else:
 		return 'discovery'
 
+# 判断是画板还是发现的标志
 is_board_or_discovery_tag = is_board_or_discovery(main_page)
 
 
 def get_first_page_urls(url, tag):
+	"""下载时，第一页和其它页分开
+	此函数获取第一页图片的下载地址
+	:param url: 第一页url
+	:param tag: board还是discovery
+	:return: 第一页最后一张图片的id
+	"""
+	# 请求json数据
 	first_page_response = requests.get(url, headers=my_header)
 	first_page_response.encoding='utf-8'
 	first_page_json_result = first_page_response.json()
-	
+
+	# 画板和发现的json格式不一样，需要分开处理
 	if tag == 'discovery':
 		first_page_pins = first_page_json_result['pins']
 	elif tag == 'board':
 		first_page_pins = first_page_json_result['board']['pins']
 
 	for i in range(len(first_page_pins)):
+		# 获取图片真实下载地址，并添加到all_urls中
 		url = pic_url_l + first_page_pins[i]['file']['key']
 		all_urls.append(url)
 
+		# 如果超过用户指定的max值，则结束
 		if len(all_urls) >= pic_max:
 			return
 
 	first_page_next_url_id = first_page_pins[-1]['pin_id']
+
 	return first_page_next_url_id
 
 
 def get_all_urls(url, tag):
+	"""下载时，第一页和其它页分开
+	此函数获取所有页面图片的下载地址
+	:param url: 下一页的url
+	:param tag: board还是discovery
+	"""
 
+	# 当页面超过一页时，先下载第一页，并获得第一页最后的id值
 	first_page_next_url_id =  get_first_page_urls(url, tag)
 
-	print(first_page_next_url_id)
+	# print(first_page_next_url_id)
 	
 	url = create_next_request_url(first_page_next_url_id)
 
-	print(url)
+	# print(url)
 	while True:
+		# 请求json数据
 		response = requests.get(url, headers=my_header)
-		
 		response.encoding= 'utf-8'
 		json_result = response.json()
 
+		# 画板和发现的json格式不一样，需要分开处理
 		if tag == 'discovery':
 			pins = json_result['pins']
 		elif tag == 'board':
 			pins = json_result['board']['pins']
 
 		for i in range(len(pins)):
+			# 获取图片真实下载地址，并添加到all_urls中
 			url = pic_url_l + pins[i]['file']['key']
 			all_urls.append(url)
 			
+			# 如果超过用户指定的max值，则结束
 			if len(all_urls) >= pic_max:
 				return
+
+		# 如果用户给的max值超过当前页面的图片数量，会抛出IndexError
 		try:
 			next_url_id=pins[-1]['pin_id']
 		except IndexError:
 			print('指定的pic_max值超过画板图片总数！')
 			return
-		next_page_url = create_next_request_url(next_url_id)
 		
+		# 获取下一页的url
+		next_page_url = create_next_request_url(next_url_id)
 		url = next_page_url
 		
 
 def create_next_request_url(id_num):
+	"""构造下一页的url
+	:param id_num: 每个页面url都有个独特的max值（上一页最后一张图片的id_num）
+	:return: 下一页的url
+	"""
+
 	return page_url_l + str(id_num) + page_url_r
 
 
-def download_image(image_url, path_name, img_name):
-    r = requests.get(image_url)
-    with open(f'./{path_name}/{img_name}.png', 'wb') as image_file:
-        image_file.write(r.content)
-    print(f'image_{img_name} is downloaded!')
+def download_image(img_url, path_name, img_name):
+	"""下载单张图片
+	:param img_url: 下载图片的地址
+	:param path_name: 下载图片到指定的文件夹
+	:param img:
+	"""
 
-# 根据url列表，开启多线程下载
-def download_all_by_urls(image_urls, path_name):
-    start_time = time.time()
+	r = requests.get(img_url)
+	with open(f'./{path_name}/{img_name}.png', 'wb') as image_file:
+		image_file.write(r.content)
+	print(f'image_{img_name} is downloaded!')
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        for index, image_url in enumerate(image_urls):
-            executor.submit(download_image, image_url, path_name, index)
 
-    print(f'It spends {time.time() - start_time} seconds')
+def download_all_by_urls(img_urls, path_name):
+	"""多线程下载
+	:param img_urls: 所有图片的下载地址
+	:param path_name: 下载图片到指定的文件夹
+	"""
+	start_time = time.time()
+
+	with concurrent.futures.ThreadPoolExecutor() as executor:
+		for index, image_url in enumerate(img_urls):
+			executor.submit(download_image, image_url, path_name, index)
+
+	print(f'It spends {time.time() - start_time} seconds')
 
 def main():
-	path_name = 'github_test'
+	"""主函数
+	"""
 
 	if pic_max <= 20:
 		get_first_page_urls(url=main_page, tag=is_board_or_discovery_tag)
